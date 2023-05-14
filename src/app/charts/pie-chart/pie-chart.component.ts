c09c907b77238c85718173810f13255d66289438
m-tonon <math.tonon@gmail.com>
2023-05-14 07:52:17 -0300

    feat: add piechart structure from ng2-charts

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pie-chart',
   templateUrl: './pie-chart.component.html',
   styleUrls: ['./pie-chart.component.css']
 })
-export class PieChartComponent {
+
+export class PieChartComponent implements OnInit{
+  
+  constructor() { }
+
+  pieChartData = [{ data: [350, 450, 120]}];
+  pieChartLabels: string[] = ['XYZ Logistics', 'Main St Bakery', 'Acme Hosting'];
+  
+  ngOnInit(): void {
+  }
 
 }
