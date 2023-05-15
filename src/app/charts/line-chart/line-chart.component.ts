f817d0e30ad30a129283fc94f64befa9ab407f83
m-tonon <math.tonon@gmail.com>
2023-05-15 07:28:33 -0300

    feat: add and style line chart

--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+const LINE_CHART_SAMPLE_DATA: any[] = [
+  {
+    data: [32, 14, 46, 23, 38, 56],
+    label: 'Sentiment Analysis',
+    fill: true,
+    tension: 0.5,
+    borderWidth: 1.5,
+    backgroundColor: 'rgba(6, 214, 160, 0.2)',
+    borderColor: 'rgba(0, 200, 140, 0.5)',
+    pointBackgroundColor: '#000',
+    pointBorderColor: '#000',
+    pointHoverBackgroundColor: '#555',
+    pointHoverBorderColor: '#555',
+    pointRadius: 3,
+  },
+  {
+    data: [12, 18, 26, 13, 28, 26],
+    label: 'Image Recognition',
+    fill: true,
+    tension: 0.5,
+    borderWidth: 1.5,
+    backgroundColor: 'rgba(255, 209, 102, 0.2',
+    borderColor: 'rgba(240, 180, 89, 0.5)',
+    pointBackgroundColor: '#000',
+    pointBorderColor: '#000',
+    pointHoverBackgroundColor: '#555',
+    pointHoverBorderColor: '#555',
+    pointRadius: 3,
+  },
+  {
+    data: [52, 34, 49, 53, 68, 62],
+    label: 'Forecasting',
+    fill: true,
+    tension: 0.5,
+    borderWidth: 1.5,
+    backgroundColor: 'rgba(15, 78, 133, 0.2)',
+    borderColor: 'rgba(3, 64, 128, 0.5)',
+    pointBackgroundColor: '#000',
+    pointBorderColor: '#000',
+    pointHoverBackgroundColor: '#555',
+    pointHoverBorderColor: '#555',
+    pointRadius: 3,
+  },
+];
+
+const LINE_CHART_LABELS: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
 
 @Component({
   selector: 'app-line-chart',
   templateUrl: './line-chart.component.html',
-  styleUrls: ['./line-chart.component.css']
+  styleUrls: ['./line-chart.component.css'],
 })
-export class LineChartComponent {
+export class LineChartComponent implements OnInit {
+  constructor() {}
 
+  lineChartData: any[] = LINE_CHART_SAMPLE_DATA;
+  lineChartLabels: string[] = LINE_CHART_LABELS;
+  lineChartOptions: any = {
+    responsive: true,
+  };
+  lineChartLegend = true;
+
+  ngOnInit() {}
 }
