9d05d73ec21a03352484622b0d4446ab849f958f
m-tonon <math.tonon@gmail.com>
2023-05-16 06:42:51 -0300

    feat: structure health component

--- src/app/pages/health/health.component.ts
+++ src/app/pages/health/health.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Server } from '../../models/server.model';
 
+const SAMPLE_SERVERS = [
+  { id: 1, name: 'dev-web', isOnline: true },
+  { id: 2, name: 'dev-mail', isOnline: false },
+  { id: 3, name: 'prod-web', isOnline: true },
+  { id: 4, name: 'prod-mail', isOnline: true }
+]
 @Component({
   selector: 'app-health',
   templateUrl: './health.component.html',
-  styleUrls: ['./health.component.css']
+  styleUrls: ['./health.component.css'],
 })
-export class HealthComponent {
+export class HealthComponent implements OnInit {
+  constructor() {}
 
+  servers: Server[] = SAMPLE_SERVERS;
+
+  ngOnInit(): void {}
 }
