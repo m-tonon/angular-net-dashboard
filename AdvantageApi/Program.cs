59069a94ef5aa27467ab8be5466dea6005686498
m-tonon <math.tonon@gmail.com>
2023-05-18 08:02:31 -0300

    feat: configuring postrettgre into the api

--- AdvantageApi/Program.cs
+++ AdvantageApi/Program.cs
@@ -1,4 +1,8 @@
+using AdvantageApi.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var startup = new Startup(builder.Configuration);
 
 // Add services to the container.
 
@@ -7,7 +11,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+startup.ConfigureServices(builder.Services); // calling ConfigureServices method
 var app = builder.Build();
+startup.Configure(app, builder.Environment); // calling Configure method
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -23,3 +29,40 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+public class Startup {
+    private string _connectionString = null;
+
+    public IConfiguration configRoot {
+        get;
+    }
+    public Startup(IConfiguration configuration) {
+        configRoot = configuration;
+    }
+    public void ConfigureServices(IServiceCollection services) {
+        services.AddRazorPages();
+
+        _connectionString = configRoot["secretConnectionString"];
+
+        services.AddMvc();
+
+        
+        services.AddDbContext<ApiContext>(
+            opt => opt.UseNpgsql(_connectionString));
+
+
+    }
+    public void Configure(WebApplication app, IWebHostEnvironment env) {
+        if (!app.Environment.IsDevelopment()) {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.MapRazorPages();
+        app.Run();
+    }
+}
