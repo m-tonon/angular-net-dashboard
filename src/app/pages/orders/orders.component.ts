6ab65d3c712b9dbeb009516d118438976d4b9c71
m-tonon <math.tonon@gmail.com>
2023-05-28 06:55:48 -0300

    feat: get orders from database

--- src/app/pages/orders/orders.component.ts
+++ src/app/pages/orders/orders.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Order } from '../../models/order.model';
+import { SalesDataService } from 'src/app/services/sales-data.service';
 
 @Component({
   selector: 'app-orders',
@@ -7,37 +8,82 @@ import { Order } from '../../models/order.model';
   styleUrls: ['./orders.component.css'],
 })
 export class OrdersComponent implements OnInit {
-  
-  constructor() {}
+  constructor(private _salesData: SalesDataService) {}
 
   orders: Order[] = [
     {
       id: 1,
-      customer: { id: 1, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},
-      total: 230, placed: new Date(2022, 12, 1),fulfilled: new Date(2022, 12, 2)
+      customer: {
+        id: 1,
+        name: 'Main St Bakery',
+        state: 'CO',
+        email: 'mainst@example.com',
+      },
+      total: 230,
+      placed: new Date(2022, 12, 1),
+      fulfilled: new Date(2022, 12, 2),
     },
     {
       id: 2,
-      customer: { id: 1, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},
-      total: 230, placed: new Date(2022, 12, 1),fulfilled: new Date(2022, 12, 2)
+      customer: {
+        id: 1,
+        name: 'Main St Bakery',
+        state: 'CO',
+        email: 'mainst@example.com',
+      },
+      total: 230,
+      placed: new Date(2022, 12, 1),
+      fulfilled: new Date(2022, 12, 2),
     },
     {
       id: 3,
-      customer: { id: 1, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},
-      total: 230, placed: new Date(2022, 12, 1),fulfilled: new Date(2022, 12, 2)
+      customer: {
+        id: 1,
+        name: 'Main St Bakery',
+        state: 'CO',
+        email: 'mainst@example.com',
+      },
+      total: 230,
+      placed: new Date(2022, 12, 1),
+      fulfilled: new Date(2022, 12, 2),
     },
     {
       id: 4,
-      customer: { id: 1, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},
-      total: 230, placed: new Date(2022, 12, 1),fulfilled: new Date(2022, 12, 2)
+      customer: {
+        id: 1,
+        name: 'Main St Bakery',
+        state: 'CO',
+        email: 'mainst@example.com',
+      },
+      total: 230,
+      placed: new Date(2022, 12, 1),
+      fulfilled: new Date(2022, 12, 2),
     },
     {
       id: 5,
-      customer: { id: 1, name: 'Main St Bakery', state: 'CO', email: 'mainst@example.com'},
-      total: 230, placed: new Date(2022, 12, 1),fulfilled: new Date(2022, 12, 2)
-    }
+      customer: {
+        id: 1,
+        name: 'Main St Bakery',
+        state: 'CO',
+        email: 'mainst@example.com',
+      },
+      total: 230,
+      placed: new Date(2022, 12, 1),
+      fulfilled: new Date(2022, 12, 2),
+    },
   ];
+  total = 0;
+  page = 1;
+  limit = 10;
 
-  ngOnInit(): void {}
-  
+  ngOnInit(): void {
+    this.getOrders();
+  }
+
+  getOrders(): void {
+    this._salesData.getOrders(this.page, this.limit)
+      .subscribe((res: any) => {
+        this.orders = res.page.data;
+      })
+  }
 }
